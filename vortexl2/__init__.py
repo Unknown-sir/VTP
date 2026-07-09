5a118cf4e8bac15f913b1e51eec2a55f02f52df7
Mr.Rskhi <93010742+Unknown-sir@users.noreply.github.com>
2026-07-10 02:41:22 +0330

    Delete vortexl2 directory

--- vortexl2/__init__.py
+++
@@ -1,3 +0,0 @@
-"""VTP Panel Edition."""
-
-__version__ = "4.3.0-vtp"
--- vortexl2/tcp_optimizer.py
+++
@@ -1,2 +0,0 @@
-def setup_tcp_optimization():
-    return True, "TCP optimization skipped in panel edition compatibility mode."
